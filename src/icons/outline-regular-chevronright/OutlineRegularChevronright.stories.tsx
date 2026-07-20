447b8a4e9401eb77a511928517fb4b618fcd2eac
Gr.Daria <57507502+D-2-G-8@users.noreply.github.com>
2026-07-21 02:39:38 +0300

    Generate OutlineRegularChevronright from Figma

---
+++ src/icons/outline-regular-chevronright/OutlineRegularChevronright.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { OutlineRegularChevronright as Component } from "./OutlineRegularChevronright";
+
+const meta: Meta<typeof Component> = {
+  title: "Icons/OutlineRegularChevronright",
+  component: Component,
+  args: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Component>;
+
+export const Default: Story = {};
---
+++ src/icons/outline-regular-chevronright/OutlineRegularChevronright.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./OutlineRegularChevronright.module.scss";
+
+export interface OutlineRegularChevronrightProps
+  extends React.SVGAttributes<SVGSVGElement> {}
+
+export function OutlineRegularChevronright({
+  ...props
+}: OutlineRegularChevronrightProps) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        className={styles.icon}
+        d="M9 5L16 12L9 19"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
