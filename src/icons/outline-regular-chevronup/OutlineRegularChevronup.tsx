1ea1f5e9a2e1d69e32771f0a7bc703ae4145e274
Gr.Daria <57507502+D-2-G-8@users.noreply.github.com>
2026-07-21 10:34:23 +0300

    Generate OutlineRegularChevronup from Figma

--- src/icons/outline-regular-chevronup/OutlineRegularChevronup.tsx
+++ src/icons/outline-regular-chevronup/OutlineRegularChevronup.tsx
@@ -4,9 +4,9 @@ import styles from "./OutlineRegularChevronup.module.scss";
 export interface OutlineRegularChevronupProps
   extends React.SVGAttributes<SVGSVGElement> {}
 
-export const OutlineRegularChevronup: React.FC<OutlineRegularChevronupProps> = (
-  props
-) => {
+export function OutlineRegularChevronup({
+  ...props
+}: OutlineRegularChevronupProps) {
   return (
     <svg
       width="24"
@@ -20,8 +20,8 @@ export const OutlineRegularChevronup: React.FC<OutlineRegularChevronupProps> = (
         className={styles.icon}
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M11.2929 8.29289C11.6834 7.90237 12.3166 7.90237 12.7071 8.29289L18.7071 14.2929C19.0976 14.6834 19.0976 15.3166 18.7071 15.7071C18.3166 16.0976 17.6834 16.0976 17.2929 15.7071L12 10.4142L6.70711 15.7071C6.31658 16.0976 5.68342 16.0976 5.29289 15.7071C4.90237 15.3166 4.90237 14.6834 5.29289 14.2929L11.2929 8.29289Z"
+        d="M12 8.29289L5.70711 14.5858C5.31658 14.9763 4.68342 14.9763 4.29289 14.5858C3.90237 14.1953 3.90237 13.5621 4.29289 13.1716L11.2929 6.17157C11.6834 5.78105 12.3166 5.78105 12.7071 6.17157L19.7071 13.1716C20.0976 13.5621 20.0976 14.1953 19.7071 14.5858C19.3166 14.9763 18.6834 14.9763 18.2929 14.5858L12 8.29289Z"
       />
     </svg>
   );
-};
+}
