506cbd7c8cc9d6aa9a6ae40b7e67f2151c951dd7
Gr.Daria <57507502+D-2-G-8@users.noreply.github.com>
2026-07-21 11:18:17 +0300

    Generate N24OutlineOrders from Figma

--- src/icons/24-outline-orders/N24OutlineOrders.stories.tsx
+++ src/icons/24-outline-orders/N24OutlineOrders.stories.tsx
@@ -4,7 +4,6 @@ import { N24OutlineOrders as Component } from "./N24OutlineOrders";
 const meta: Meta<typeof Component> = {
   title: "Icons/N24OutlineOrders",
   component: Component,
-  args: {},
 };
 export default meta;
 
